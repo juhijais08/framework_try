315793272adcc0dcf30d3642062bca6b2edd9482
Juhi Jaiswal <juhijais08@yahoo.co.in>
2025-04-20 11:55:20 +0530

    my first commit

---
+++ src/main/java/page_objects/LoginPageObject.java
@@ -0,0 +1,45 @@
+package page_objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPageObject {
+	
+	WebDriver driver;
+		
+	public LoginPageObject(WebDriver driver) {
+ 
+	this.driver=driver;
+	}
+
+	public void dologin()
+	
+	{
+		WebElement email = driver.findElement(By.id("email"));
+		WebElement pass = driver.findElement(By.id("pass"));
+		WebElement lgbutton = driver.findElement(By.name("login"));
+		email.sendKeys("admin123");
+		pass.sendKeys("admin1234");
+		lgbutton.click();	
+	}
+	
+	public void launchapp()
+	
+	{
+		driver.get("https://www.facebook.com/");
+	}
+	
+     public void launchandlogin() {
+    	 driver.get("https://www.facebook.com/");
+ 		WebElement email = driver.findElement(By.id("email"));
+ 		WebElement pass = driver.findElement(By.id("pass"));
+ 		WebElement lgbutton = driver.findElement(By.name("login"));
+ 		email.sendKeys("admin123");
+ 		pass.sendKeys("admin1234");
+ 		lgbutton.click();  
+	}
+
+      public void
+}
+
---
+++ src/test/java/testscript2/Basetest.java
@@ -0,0 +1,27 @@
+package testscript2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import page_objects.LoginPageObject;
+
+public class Basetest {
+         @Test
+
+	public void tc01(){
+	
+		WebDriver driver = new ChromeDriver();
+		
+
+		LoginPageObject lpo= new LoginPageObject(driver);
+		lpo.launchapp();
+		lpo.dologin();
+		
+	}
+		
+
+	}
+
+
